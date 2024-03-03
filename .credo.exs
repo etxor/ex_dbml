96f20d6b5b6c7f631c34ce5c5ec4d3405561345b
etxor <159087894+etxor@users.noreply.github.com>
2024-03-03 01:32:58 -0600

    fix(ExDbml): fix CI steps

--- .credo.exs
+++ .credo.exs
@@ -121,7 +121,7 @@
           #
           {Credo.Check.Refactor.Apply, []},
           {Credo.Check.Refactor.CondStatements, []},
-          {Credo.Check.Refactor.CyclomaticComplexity, []},
+          {Credo.Check.Refactor.CyclomaticComplexity, [max_complexity: 5]},
           {Credo.Check.Refactor.FilterCount, []},
           {Credo.Check.Refactor.FilterFilter, []},
           {Credo.Check.Refactor.FunctionArity, []},
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExDbml.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.0-dev"
 
   def project do
     [
@@ -9,7 +9,23 @@ defmodule ExDbml.MixProject do
       version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Dialyxir config
+      dialyzer: [
+        # Put the project-level PLT in the priv/ directory (instead of the default _build/ location)
+        plt_file: {:no_warn, "priv/plts/project.plt"}
+      ],
+
+      # ExDoc config
+      name: "ExDbml",
+      source_url: "https://github.com/etxor/ex_dbml",
+      # homepage_url: "https://some.url"
+      docs: [
+        main: "ExDbml",
+        # logo: "path/to/logo.png",
+        extras: ["README.md", "LICENSE"]
+      ]
     ]
   end
 
@@ -24,7 +40,9 @@ defmodule ExDbml.MixProject do
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 end
